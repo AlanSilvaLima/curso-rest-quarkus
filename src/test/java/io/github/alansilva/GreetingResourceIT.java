cd5e5d4bb6510d3c49207e2c67dd683311cd339f
Alan Silva <alan.sousa@cittamobi.com.br>
2025-06-26 10:50:04 -0300

    first commit

---
+++ src/test/java/io/github/alansilva/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package io.github.alansilva;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
