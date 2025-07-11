819142413b970fef256b1c72905b17b56b48deeb
Alan Silva <alan.sousa@cittamobi.com.br>
2025-07-10 22:53:47 -0300

    Alterando os codigos de status para mais especificos

--- src/main/java/io/github/alansilva/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/alansilva/quarkussocial/rest/UserResource.java
@@ -38,8 +38,10 @@ public class UserResource {
 
         Set<ConstraintViolation<CreateUserRequest>> violations = validator.validate(userRequest);
         if (!violations.isEmpty()){
-            ResponseError responseError = ResponseError.createFromValidation(violations);
-            return Response.status(400).entity(responseError).build();
+
+            return ResponseError
+                    .createFromValidation(violations)
+                    .withStatusCode(ResponseError.UNPROCESSABLE_ENTITY_STATUS);
         }
 
         User user = new User();
@@ -47,7 +49,10 @@ public class UserResource {
         user.setName(userRequest.getName());
 
         repository.persist(user);
-        return Response.ok(user).build();
+        return Response
+                .status(Response.Status.CREATED.getStatusCode())
+                .entity(user)
+                .build();
     }
 
     @GET
@@ -63,7 +68,7 @@ public class UserResource {
         User user= repository.findById(id);
         if (user !=null) {
             repository.delete(user);
-            return Response.ok().build();
+            return Response.noContent().build();
         }
 
         return Response.status(Response.Status.NOT_FOUND).build();
@@ -78,7 +83,7 @@ public class UserResource {
         if (user !=null){
             user.setName(userData.getName());
             user.setAge(userData.getAge());
-            return  Response.ok().build();
+            return  Response.noContent().build();
         }
 
         return  Response.status(Response.Status.NOT_FOUND).build();
--- src/main/java/io/github/alansilva/quarkussocial/rest/dto/ResponseError.java
+++ src/main/java/io/github/alansilva/quarkussocial/rest/dto/ResponseError.java
@@ -3,6 +3,7 @@ package io.github.alansilva.quarkussocial.rest.dto;
 import jakarta.validation.Constraint;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintViolation;
+import jakarta.ws.rs.core.Response;
 
 import java.util.Collection;
 import java.util.List;
@@ -11,6 +12,8 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class ResponseError {
+
+    public  static final int UNPROCESSABLE_ENTITY_STATUS = 422;
 
     private String message;
     private Collection<FieldError> error;
@@ -45,5 +48,9 @@ public class ResponseError {
 
     public void setError(Collection<FieldError> error) {
         this.error = error;
+    }
+
+    public Response withStatusCode(int code){
+        return Response.status(code).entity(this).build();
     }
 }
